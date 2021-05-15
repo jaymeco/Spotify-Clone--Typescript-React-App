c22e3a2af009502f6c238adf6ebd4304870ff8a7
jaymeco <jayme.anunciacao@gmail.com>
2021-05-15 16:29:11 -0300

    Add back button in header component

--- src/pages/Dashboard/components/Header/index.tsx
+++ src/pages/Dashboard/components/Header/index.tsx
@@ -1,14 +1,28 @@
 import logo from '../../../../assets/images/logo.svg';
+import { IoIosArrowBack } from 'react-icons/io';
+import { useHistory } from 'react-router-dom';
 import SearchBar from '../SearchBar';
 import './style.css';
 
 export default function Header() {
+  const navigation = useHistory();
+
   return (
     <>
       <header className="main-header">
         <div>
+          {
+            navigation.location.pathname !== '/home'? (
+              <button 
+                onClick={()=>navigation.goBack()}
+                className="back-button"
+              >
+                <IoIosArrowBack size={25} color="#fff" />
+              </button>
+            ): null
+          }
           <img src={logo} alt="Logo Spotify" />
-          <SearchBar/>          
+          <SearchBar />
         </div>
         <h3>Playlists</h3>
       </header>
