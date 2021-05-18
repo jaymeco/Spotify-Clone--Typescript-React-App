9c6b95b8f71c1366fa64c2d365b2ca0d9c777b02
jaymeco <jayme.anunciacao@gmail.com>
2021-05-18 12:14:26 -0300

    Create loading componen

---
+++ src/pages/components/Loading/index.tsx
@@ -0,0 +1,13 @@
+import './style.css';
+
+export function Loading(){
+  return (
+    <div className="loading-container">
+      <div className="loading-effect">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  )
+}
